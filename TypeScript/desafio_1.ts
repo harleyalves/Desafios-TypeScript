9276464873e5a695a1bd6105b11d66520915b469
harleyalves <75190014+harleyalves@users.noreply.github.com>
2022-06-27 21:25:39 -0300

    Add files via upload

---
+++ TypeScript/desafio_1.ts
@@ -0,0 +1,8 @@
+
+let funcionario = {
+    code:10,
+    name:"John"
+};
+
+console.log(funcionario.code);
+console.log(funcionario.name);
---
+++ TypeScript/desafio_2.ts
@@ -0,0 +1,31 @@
+// Como podemos melhorar o esse código usando TS? 
+interface pessoa {
+    nome:string;
+    idade:number;
+    profissao:string;
+}
+
+let maria : pessoa = {
+    nome : "maria",
+    idade : 29,
+    profissao :"atriz"  
+};
+
+
+let roberto : pessoa = {
+    nome : "roberto",
+    idade : 19,
+    profissao :"Padeiro"  
+}
+
+let laura : pessoa = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let carlos : pessoa = {
+    nome : "carlos",
+    idade : 19,
+    profissao : "padeiro"
+}
